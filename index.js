5818fd04dcbfd60c52d7af212cf8fc00bb7b2bca
kilroyda <kilroyda@tcd.ie>
2018-11-07 20:55:05 +0000

    added default GET request

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-module.exports = function() {
-  return 'hello';
+var https = require("https");
+var fs = require("fs");
+var exports = module.exports = {};
+
+exports.getDefault = function(){
+
+    var options = {
+        hostname: 'api.github.com',
+        port: 443,
+        path: '/repos/kilroyda/Github_REST_Access/git/commits',
+        method: 'GET',
+        message: 'arbitrary',
+        headers: {
+            'User-Agent': 'Awesome-Octocat-App'
+        }
+
+    };
+
+    var responseString = "";
+    var req = https.request(options, function (res) {
+        console.log('statusCode: ', res.statusCode);
+        console.log('headers: ', res.headers);
+
+        res.on('data', function (data) {
+            console.log(responseString += data);
+        });
+        res.on("end", function () {
+            console.log(responseString);
+        });
+    });
+
+    req.end();
+    req.on('error', (e) => {
+        console.error(e);
+    });
+    return responseString;
 }
+
+exports.sayHello = function(){
+    return 'hello';
+};
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,8 +1,21 @@
 const assert = require('chai').assert;
 const index = require('../index');
+const app = require('../index.js');
 
 describe('index', function(){
-  it('app should return hello', function(){
-    assert.equal(index(), 'hello');
+  it('sayHello should return hello', function(){
+      let result = app.sayHello();
+      assert.equal(result, 'hello');
+  });
+
+  it('sayHello should return type string', function(){
+        let result = app.sayHello();
+        assert.typeOf(result, 'string');
+  });
+
+  it('n', function(){
+      let result = app.getDefault();
+      console.log("\nYoke:", result);
+      assert.equal(result.statusCode, 403);
   });
 });
