e047d78f8cddb6607c10ffa6b9ad7f391a6cb8bb
kilroyda <kilroyda@tcd.ie>
2018-11-11 18:04:16 +0000

    Created addUser() to add a user to the database

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var fs = require("fs");
 var github = require("octonode");
 
 //Modal Imports
-var User = require('../model/user.js');
+var User = require('./model/user.js');
 
 //Exports
-var exports = module.exports = {};
+//var exports = module.exports = {};
 
 //---------------GET Default---------------//
 exports.getDefault = function(request, response){
@@ -27,39 +27,58 @@ exports.getDefault = function(request, response){
         //console.log(stringResult);
         //console.log("Headers= " + JSON.stringify(headers));
         //console.log("Body= " + bodyText);
-    });
 
+        // var options = {
+        //     hostname: 'api.github.com',
+        //     port: 443,
+        //     path: '/repos/kilroyda/Github_REST_Access/git/commits',
+        //     method: 'GET',
+        //     message: 'arbitrary',
+        //     headers: {
+        //         'User-Agent': 'Awesome-Octocat-App'
+        //     }
+        //
+        // };
+        //
+        // var responseString = "";
+        // var req = https.request(options, function (res) {
+        //     console.log('statusCode: ', res.statusCode);
+        //     console.log('headers: ', res.headers);
+        //
+        //     res.on('data', function (data) {
+        //         console.log(responseString += data);
+        //     });
+        //     res.on("end", function () {
+        //         console.log(responseString);
+        //     });
+        // });
+        //
+        // req.end();
+        // req.on('error', (e) => {
+        //     console.error(e);
+        // });
+        // return responseString;
+    });
 }
-    // var options = {
-    //     hostname: 'api.github.com',
-    //     port: 443,
-    //     path: '/repos/kilroyda/Github_REST_Access/git/commits',
-    //     method: 'GET',
-    //     message: 'arbitrary',
-    //     headers: {
-    //         'User-Agent': 'Awesome-Octocat-App'
-    //     }
-    //
-    // };
-    //
-    // var responseString = "";
-    // var req = https.request(options, function (res) {
-    //     console.log('statusCode: ', res.statusCode);
-    //     console.log('headers: ', res.headers);
-    //
-    //     res.on('data', function (data) {
-    //         console.log(responseString += data);
-    //     });
-    //     res.on("end", function () {
-    //         console.log(responseString);
-    //     });
-    // });
-    //
-    // req.end();
-    // req.on('error', (e) => {
-    //     console.error(e);
-    // });
-    // return responseString;
+
+//---------------New User---------------//
+exports.addUser = function(request, response){
+    var user = new User();
+    user.login = request.body.login;
+    user.id = request.body.id;
+    user.followers = request.body.followers;
+    user.following = request.body.following;
+    user.repo_count = request.body.public_repos;
+    user.repos_url = request.body.repos_url;
+
+    // login: {type: String, required: true},
+    // id: {type: Number, required: true, unique: true},
+    // public_repos: {type: Number, required: true},
+    // followers: {type: Number, required: true},
+    // following: {type: Number, required: true},
+    // repos_url: {type: String, required: true}
+}
+
 
 
 exports.sayHello = function(){
--- model/user.js
+++ model/user.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 var UserSchema = new Schema({
     login: {type: String, required: true},
     id: {type: Number, required: true, unique: true},
-    public_repos: {type: Number, required: true},
+    repo_count: {type: Number, required: true},
     followers: {type: Number, required: true},
     following: {type: Number, required: true},
     repos_url: {type: String, required: true}
